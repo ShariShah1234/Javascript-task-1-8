c4e308116c18d61ff4b7f57471cd54b375cc9b8b
ShariShah1234 <muhammadshaheryar74@gmail.com>
2022-11-01 23:21:37 +0500

    comment

---
+++ app.js
@@ -0,0 +1,119 @@
+//chapter no: 1 "alert"
+// Task no :1
+alert("Thanks for your input!");
+alert("thanks for your visit my webiste");
+alert("hello user");
+window.alert("Wellcome");
+alert ( "Thanks for your input!" );
+
+
+//chapter no: 2 "declare variable"
+// Task no :2
+
+var nationality;
+nationality = "pakistani";
+
+
+
+var z = "Mark";
+var c = "Mark";
+var x = "Mark";
+
+
+var x="shari";
+var y="shari";
+var c="shari":;
+
+
+//chapter no: 3 "Variables for Numbers"
+// Task no :3
+var newNum = 23;
+var numAdd = 7;
+var newNum = newNum + numAdd;
+
+
+var originalNum = "33";
+var newNum = originalNum + 11;
+
+
+
+var shaheryar= 177;
+alert(shaheryar);
+
+//chapter no: 4 "Math expressions:
+//Familiar operators"
+// Task no :4
+
+var otherNumber = 2 + 2;
+alert(othernumber);
+
+
+var otherNumber = 2 - 2;
+alert(othernumber);
+
+
+var otherNumber = 2 / 2;
+alert(othernumber);
+
+
+var otherNumber = 2 * 2;
+alert(othernumber);
+
+
+//chapter no: 5 ":
+//"
+// Task no :5
+
+
+var num = 5;
+var newNum = num++;
+
+var num = 6;
+var newNum = ++num;
+
+
+var num = 7;
+var newNum = num--;
+
+var num = 8;
+var newNum = --num;
+
+
+//chapter no: 7 ":
+//"
+// Task no :7
+
+var totalAverage = (1 + 3) * 4;
+
+var totalSum = (3 / 5) * 9;
+
+
+var totalSum = (9 - 8) / 2;
+
+//chapter no: 8 ":
+//"
+// Task no :8
+var message = "Think before you speak";
+alert(message);
+
+var message = "come back later";
+alert(message);
+
+
+var message = "Thanks for visiting my website";
+alert(message);
+
+
+
+
+1 var message = "Thanks,";
+2 var userName = "User";
+3 var ajmal = "!";
+4 var usermessage = message + userName + ajmal;
+5 alert(usermessage);
+
+
+
+alert("6" + "6");
+//equal  66
+//string concatention
